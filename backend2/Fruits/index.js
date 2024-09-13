b537e3954bf285880455942a6546799200ea047c
Lalit Kumar Sahu <153749747+LalitKumarSahu@users.noreply.github.com>
2024-09-13 11:41:23 +0530

    Add files via upload

---
+++ backend2/Fruits/index.js
@@ -0,0 +1,9 @@
+// const apple = require("./apple");
+// const bnana = require("./bnana");
+// const orange = require("./orange");
+
+// let fruits = ["apple", "bnana","orange"];
+// module.exports = fruits;
+
+
+
---
+++ backend2/script.js
@@ -0,0 +1,60 @@
+let n = 5;
+// console.log(n);
+
+// for(let i= 0; i<=n; i++){
+//   console.log("hello = ", i);
+// }
+//--------------------------------------->
+// let args = process.argv;
+// for(let i=2; i<args.length; i++){
+//   console.log("hello to",args[i]);
+// }
+
+//--------------------------------------->
+    
+// const math = require("./math");
+// //console.log(someVal);
+// console.log(math.sum(4,5));
+// console.log(math.mul(4,5));
+// console.log(math.divd(14,5));
+// console.log(math.pi);
+
+//--------------------------------------->
+
+// const Info = require("./Fruits");
+// console.log(Info);
+// console.log(Info[0].name);
+
+//--------------------------------------->
+
+// const figlet = require("figlet");
+
+// figlet("Hello World!!", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+//--------------------------------------->
+
+// import {sum, pi} from "./math.js";
+// console.log(sum(1,3));
+// console.log(pi);
+
+//--------------------------------------->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
